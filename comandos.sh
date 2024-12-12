45ccaa58ac13ea0e46bc28beac1bc6bca654c7cd
Beatriz Alves <89867185+beatrizalves20@users.noreply.github.com>
2024-12-12 16:02:30 -0300

    Delete comandos.sh

--- comandos.sh
+++
@@ -1,8 +0,0 @@
-# subir o lb
-docker run -it -p 80:80 -v ./default.conf:/etc/nginx/conf.d/default.conf --name loadbalancer nginx:alpine
-
-#subir os nós
-docker run -d --name node1 nginx:alpine
-docker run -d --name node2 nginx:alpine
-docker run -d --name node3 nginx:alpine
-docker run -it --name node1 nginx:alpine
